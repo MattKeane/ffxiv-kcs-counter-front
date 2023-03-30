07566fd2fdd4c2f794db7ac7e3427edc5bbd5857
Matt Keane <mattpkeane@gmail.com>
2023-03-30 13:49:21 -0500

    initial set up for react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
