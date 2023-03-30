eab5d08f486a1730827fbb0192a36fd18058f1da
Matt Keane <mattpkeane@gmail.com>
2023-03-30 14:01:36 -0500

    adds list of links to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,48 @@
+import { Link } from 'react-router-dom';
+import './style.css';
+
 export default function Home() {
-    return <h1>Home Page</h1>;
+    const sRanks = [
+        {
+            name: 'Sphatika',
+            link: '/sphatika',
+        },
+        {
+            name: 'Ruminator',
+            link: '/ruminator',
+        },
+        {
+            name: 'Ixtab',
+            link: '/ixtab',
+        },
+        {
+            name: 'Okina',
+            link: '/okina',
+        },
+        {
+            name: 'Leucrotta',
+            link: '/leucrotta',
+        },
+        {
+            name: 'Minhocao',
+            link: '/minhocao',
+        },
+    ];
+
+    const sRankLinks = sRanks.map(sRank => (
+        <li>
+            <Link to={ sRank.link }>
+                { sRank.name }
+            </Link>
+        </li>)
+    );
+
+    return (
+        <>
+            <h2>Select an S Rank to Attempt</h2>
+            <ul>
+                { sRankLinks }
+            </ul>
+        </>
+    );
 }
