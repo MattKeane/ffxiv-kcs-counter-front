6357a05380e44be11697d77999b6c7ca20aab6b1
Matt Keane <mattpkeane@gmail.com>
2023-04-02 17:12:11 -0500

    adds socket.io and join room functionality

--- src/pages/Room/index.js
+++ src/pages/Room/index.js
@@ -1,7 +1,20 @@
+import {
+    useRef,
+    useEffect,
+} from 'react';
 import { useParams } from 'react-router-dom';
+import { io } from 'socket.io-client';
 
 export default function Room() {
+    const socket = useRef(null);
     const { room } = useParams();
+    
+    useEffect(() => {
+        socket.current = io('/spawn');
+        socket.current.emit('joinRoom', room, res => {
+            console.log(res);
+        });
+    }, [room])
 
     return <h3>{ room }</h3>;
 }
