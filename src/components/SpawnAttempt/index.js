4d45a21c9409e352b16368d8564f59e5391effae
Matt Keane <mattpkeane@gmail.com>
2023-04-04 15:31:34 -0500

    gives mob default counts for each lap

--- src/components/SpawnAttempt/index.js
+++ src/components/SpawnAttempt/index.js
@@ -7,11 +7,31 @@ export default function SpawnAttempt({ name, mobs, room }) {
     // name prop is the name of the S rank to be spawned
     // mobs prop is an array of objects representing each mob required for the spawn
 
+    const defaultLaps = {
+        Asvattha: 11,
+        Vajralangula: 12,
+        Pisaca: 11,
+        Thinker: 9,
+        Wanderer: 15,
+        Weeper: 8,
+        'Cracked Ronkan Doll': 12,
+        'Cracked Ronkan Thorn': 11,
+        'Cracked Ronkan Vessel': 9,
+        Leshy: 14,
+        Diakka: 9,
+        Yumemi: 3,
+        'Naked Yumemi': 3,
+        'Allagan Chimera': 6,
+        'Meracydian Vouivre': 10,
+        'Lesser Hydra': 11,
+        'Earth Sprite': 14,
+    };
+
     const mobCounters = mobs.map((mob, i) => (
         <MobCounter 
             key={ i }
             name={ mob.name } 
-            defaultLap={ mob.defaultLap }
+            defaultLap={ defaultLaps[mob.name] }
             startingCount={ mob.count }
             room={ room }
         />)
