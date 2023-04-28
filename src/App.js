6804283c2fc6902cb5a8eab11520a2f9166ddc8c
Matt Keane <mattpkeane@gmail.com>
2023-04-28 11:57:52 -0500

    fixes display of copyright notice

--- src/App.js
+++ src/App.js
@@ -19,6 +19,15 @@ function App() {
         <Route path="/" element={<Home message={ message } />} />
         <Route path=":room" element={ <Room setMessage={ setMessage } /> } />
       </Routes>
+      <footer>
+      <div>
+        <small>FINAL FANTASY is a registered trademark of Square Enix Holdings Co., Ltd.
+        </small>
+      </div>
+      <div>
+        <small>Background image © SQUARE ENIX CO., LTD. All Rights Reserved.</small>
+      </div>
+    </footer>
     </div>
   );
 }
