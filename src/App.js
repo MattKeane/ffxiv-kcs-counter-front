33c5c4074c7f90f6dd4ffd7c4675c0874788af1a
Matt Keane <mattpkeane@gmail.com>
2023-06-27 15:57:19 -0500

    clicking info button opens info modal

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import {
   Route 
 } from 'react-router-dom';
 
+import Backdrop from '@mui/material/Backdrop';
+
 import Home from './pages/Home';
 import Room from './pages/Room';
 
 import InfoButton from './components/InfoButton';
+import InfoModal from './components/InfoModal';
 
 import './App.css';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpen = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
 
   return (
     <div className="app">
@@ -21,7 +28,13 @@ function App() {
         <Route path="/" element={<Home message={ message } />} />
         <Route path=":room" element={ <Room setMessage={ setMessage } /> } />
       </Routes>
-      <InfoButton />
+      <InfoButton onClick={ handleOpen }/>
+      <Backdrop
+        open={ showModal }
+        onClick={ handleClose }
+      >
+        <InfoModal />
+      </Backdrop>
       <footer>
       <div>
         <small>FINAL FANTASY is a registered trademark of Square Enix Holdings Co., Ltd.
--- src/components/InfoButton/index.js
+++ src/components/InfoButton/index.js
@@ -2,10 +2,14 @@ import IconButton from '@mui/material/IconButton';
 import InfoIcon from '@mui/icons-material/Info';
 import './style.css';
 
-export default function InfoButton() {
+export default function InfoButton({ onClick }) {
     return (
         <div className="infoButtonContainer">
-            <IconButton aria-label="information" size="small">
+            <IconButton 
+                aria-label="information" 
+                size="small"
+                onClick={ onClick }
+            >
                 <InfoIcon />
             </IconButton>
         </div>
